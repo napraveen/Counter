a64024e03781ef0a0781670565fbb2ed8dc032ee
napraveen <113528770+napraveen@users.noreply.github.com>
2022-11-11 12:37:03 +0530

    Add files via upload

---
+++ counter/app.js
@@ -0,0 +1,33 @@
+let count = 0;
+const value = document.querySelector("#value");
+const  btns = document.querySelectorAll(".btn");
+
+
+
+btns.forEach((btn) =>{
+    btn.addEventListener('click', (e) =>{
+        const styles = e.currentTarget.classList
+
+        if (styles.contains('increase')){
+            count++;
+        } else if(styles.contains("decrease")){
+            count--;
+        } else{
+            count =0;
+        }
+
+        if (count>0){
+            value.style.color ='green';
+            
+        } else if(count<0){
+            value.style.color ='red';
+        } else{
+            value.style.color ='black';
+        }
+    
+        value.textContent = count;
+
+    })
+
+}
+)
